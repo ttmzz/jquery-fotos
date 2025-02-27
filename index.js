badbf39a89153be9c6509280ec924d527ae52b4e
Tito <Tito@teste.com>
2025-02-27 10:47:48 -0300

    add index.js

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
-const form = document.getElementById('comparar');
+const form = document.getElementById('formulario');
 
+let novoContato = '';
 
 form.addEventListener('submit', function(e) {
   e.preventDefault();
 
-  const CampoA = document.getElementById('A');
-  const CampoB = document.getElementById('B');
+  const Nome = document.querySelector('.nome');
+  const Tel = document.querySelector('.tel');
 
-  let A = `${CampoA.value}`;
-  let B = `${CampoB.value}`;
+  let contato = '<tr>';
+  contato += `<td>${Nome.value}</td>`;
+  contato += `<td>${Tel.value}</td>`;
+  contato += '</tr>';
 
-  const successMsg = "Positivo";
-  const errorMsg = "Negativo";
+  novoContato += contato;
 
-  if(A < B) {
-   const success = document.querySelector('.success-msg');
-   success.innerHTML = successMsg;
-   success.style.display = 'block';
-  } else {
-    const error = document.querySelector('.error-msg');
-    error.innerHTML = errorMsg;
-    error.style.display = 'block';
-  }
-
+  const Table = document.querySelector('tbody');
+  Table.innerHTML = novoContato;
 });
